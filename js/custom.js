25b39afb1d7d4c718d18d5129bcc848039a58458
Ramees Salim <rameessalim@users.noreply.github.com>
2017-04-24 18:04:17 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+// Initialize affix and add an offset to add affix class on scroll
+$('#mainNav').affix({
+  offset: {
+    top: 100
+  }
+})
+ 
+
+$(function(){
+ 
+	$(document).on( 'scroll', function(){
+ 
+		if ($(window).scrollTop() > 100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+ 
+	$('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	element = $('body');
+	offset = element.offset();
+	offsetTop = offset.top;
+	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+
